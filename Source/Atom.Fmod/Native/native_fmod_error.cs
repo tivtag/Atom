418e607d05ede3e9732ccc9682394e4e9ed6d1de
Paul Ennemoser <tivtag@gmail.com>
2021-01-14 23:17:14 +0100

    Updated Atom.Fmod to 2.01.17 and upgraded to new csproj format.

--- Source/Atom.Fmod/Native/native_fmod_error.cs
+++ Source/Atom.Fmod/Native/native_fmod_error.cs
@@ -107,4 +107,3 @@ namespace Atom.Fmod.Native
         }
     }
 }
-
--- Source/Atom.Fmod/Properties/AssemblyInfo.cs
+++ Source/Atom.Fmod/Properties/AssemblyInfo.cs
@@ -9,12 +9,7 @@
 // </author>
 
 using System;
-using System.Reflection;
-using System.Resources;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle( "Atom.Fmod" )]
-[assembly: AssemblyDescription( "A library that contains an object oriented wrapper around the C++ audio libary FMODex 4.44.56. (http://www.fmod.org/)" )]
 [assembly: Guid( "0f4a6a4e-b28a-475e-86f0-d03320f26f73" )]
-
 [assembly: CLSCompliant( false )]
